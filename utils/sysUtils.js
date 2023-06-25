ea192950285e5d229d91ffb7fb60ef41aa068157
0x0is1 <42772562+0x0is1@users.noreply.github.com>
2023-06-25 15:44:56 +0530

    [+] Non-Significant stylesheet and function changes

--- utils/sysUtils.js
+++ utils/sysUtils.js
@@ -1,9 +1,16 @@
 const Util = imports.misc.util;
+const Main = imports.ui.main;
 
 function runCommand(command){
 	Util.spawnCommandLine(command);
 }
 
+function showNotification(message) {
+  const sourceName = 'Your Extension Name'; // Replace with your extension's name
+  Main.notify(sourceName, JSON.stringify(message));
+}
+
 const exports = {
-	runCommand: runCommand
+	runCommand: runCommand,
+	showNotification: showNotification
 };
